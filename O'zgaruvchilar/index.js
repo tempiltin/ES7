9bb8db054a34ce1fe600e15403919d106ecaa033
rashidDev2001 <168219700+rashidDev2001@users.noreply.github.com>
2025-01-08 01:34:45 +0500

    ads

---
+++ O'zgaruvchilar/index.js
@@ -0,0 +1,33 @@
+// console.log(x); // undefined
+// var x = 5;
+// console.log(x); // 5
+
+// console.log(y); // ReferenceError
+// let y = 10;
+// console.log(y); // 10
+
+// if (true) {
+//   let z = 20;
+//   console.log(z); // 20
+// }
+// // console.log(z); // ReferenceError
+
+
+// const a = 15;
+// // a = 20; // TypeError
+
+// const arr = [1, 2, 3];
+// arr.push(4); // To'g'ri
+// console.log(arr); // [1, 2, 3, 4]
+
+
+// console.log(a); // undefined (o'zgaruvchi ko'tarilgan)
+// var a = 5;
+// console.log(a); // 5
+
+// console.log(b); // ReferenceError (o'zgaruvchi TDZda)
+// let b = 10;
+
+// console.log(c); // ReferenceError
+// const c = 15;
+
